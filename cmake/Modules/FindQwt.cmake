53f949bc866b908c3a617095548dba4e946407c4
Albert Kottke <albert.kottke@gmail.com>
2016-03-26 09:19:01 -0700

    Changed from QWT to Qwt.

--- cmake/Modules/FindQwt.cmake
+++ cmake/Modules/FindQwt.cmake
@@ -1,30 +1,30 @@
-# - Try to find QWT
+# - Try to find Qwt
 # Once done, this will define
 #
-#  QWT_FOUND - system has QWT
-#  QWT_INCLUDE_DIRS - the QWT include directories
-#  QWT_LIBRARIES - link these to use QWT
+#  Qwt_FOUND - system has Qwt
+#  Qwt_INCLUDE_DIRS - the Qwt include directories
+#  Qwt_LIBRARIES - link these to use Qwt
 
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(QWT_PKGCONF qwt-qt5)
+libfind_pkg_check_modules(Qwt_PKGCONF qwt)
 
 # Include dir
-find_path(QWT_INCLUDE_DIR
+find_path(Qwt_INCLUDE_DIR
   NAMES qwt.h
-  PATHS ${QWT_PKGCONF_INCLUDE_DIRS}
+  PATHS ${Qwt_PKGCONF_INCLUDE_DIRS}
   PATH_SUFFIXES qwt
 )
 
 # Finally the library itself
-find_library(QWT_LIBRARY
+find_library(Qwt_LIBRARY
   NAMES qwt
-  PATHS ${QWT_PKGCONF_LIBRARY_DIRS}
+  PATHS ${Qwt_PKGCONF_LIBRARY_DIRS}
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(QWT_PROCESS_INCLUDES QWT_INCLUDE_DIR)
-set(QWT_PROCESS_LIBS QWT_LIBRARY)
-libfind_process(QWT)
+set(Qwt_PROCESS_INCLUDES Qwt_INCLUDE_DIR)
+set(Qwt_PROCESS_LIBS Qwt_LIBRARY)
+libfind_process(Qwt)
