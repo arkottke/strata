87c0a16b94da7e2abdb24833653f4ddf7b2b36be
Albert Kottke <albert.kottke@gmail.com>
2018-10-18 08:34:05 -0700

    Travis test.

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,6 +3,8 @@
 mkdir build
 cd build
 
+echo $TRAVIS_OS_NAME
+
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then 
     cmake .. \
         -DQWT_ROOT_DIR=$QWT_ROOT_DIR \
@@ -10,7 +12,9 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX:STRING=dist
 else
+    echo "Building with QWT_INCLUDE_DIR variable."
     cmake .. \
+        -DQWT_ROOT_DIR=$QWT_ROOT_DIR \
         -DQWT_INCLUDE_DIR=$QWT_INCLUDE_DIR \
         -DGSL_ROOT_DIR=$GSL_ROOT_DIR \
         -DCMAKE_BUILD_TYPE=Release \
