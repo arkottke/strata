4c34c0f5d14faba6683d69a3d082fe7a334da036
Albert Kottke <albert.kottke@gmail.com>
2022-04-20 14:34:06 -0700

    Another attempt.

--- contrib/github-action-install-qwt.sh
+++ contrib/github-action-install-qwt.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+
+source /opt/qt515/bin/qt515-env.sh
+
+echo "Cloning Qwt"
 git clone --branch qwt-6.2 https://git.code.sf.net/p/qwt/git qwt
 cd qwt
-source /opt/qt515/bin/qt515-env.sh
-qmake --version
+echo "Configuring Qwt"
 qmake
+echo "Building Qwt"
 make -j2
+
+QWT_ROOT_DIR=`pwd`
+LD_LIBRARY_PATH=$(readlink -f lib):$LD_LIBRARY_PATH
+
+cd ..
+
+mkdir build
+cd build
+echo "Configuring Strata"
+cmake .. -DQWT_ROOT_DIR=$QWT_ROOT_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:STRING=dist
+
+echo "Building Strata"
+cmake --build . --target install
+
+./dist/usr/bin/strata -v
