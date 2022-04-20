0cf9f65229c02c15a62f2525a5b4136f86706820
Albert Kottke <albert.kottke@gmail.com>
2022-04-20 13:24:42 -0700

    Added functions for compatbility with Qt versions.

---
+++ source/QtCompatibility.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// This file is part of Strata.
+//
+// Strata is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Strata is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Strata.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2010-2022 Albert Kottke
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <QString>
+#include <QtGlobal>
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+namespace Qt {
+static auto endl = ::endl;
+static auto SkipEmptyParts = QString::SkipEmptyParts;
+} // namespace Qt
+#endif
